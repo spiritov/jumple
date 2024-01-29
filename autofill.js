b99532e0f93c53839f76b0ae95195d685a2cd220
spiritov <132454615+spiritov@users.noreply.github.com>
2024-01-29 16:48:51 -0700

    move autofill functions to autofill.js

---
+++ autofill.js
@@ -0,0 +1,153 @@
+const autofillElement = document.getElementById('autofill');
+var activeAutofillListID = 0;
+var activeAutofillListElement = document.getElementById('autofill'); //placeholder
+var autofillTerms = [];
+
+function initializeAutofill()
+{
+    inputElement.addEventListener('keydown', function (pressed) { //active element tracking
+        if(autofillTerms.length > 0)
+        {
+            if(pressed.key === 'Tab')
+            {
+                pressed.preventDefault();
+                fillAutofillWithActiveTerm();
+            }
+            else if(pressed.key === 'ArrowUp')
+            {
+                pressed.preventDefault();
+                if(activeAutofillListID > 0)
+                {
+                    setActiveAutofillElement(activeAutofillListID - 1);
+                    activeAutofillListElement.scrollIntoView();
+                }
+            }
+            else if(pressed.key === 'ArrowDown')
+            {
+                pressed.preventDefault();
+                if(activeAutofillListID < autofillTerms.length - 1)
+                {
+                    setActiveAutofillElement(activeAutofillListID + 1);
+                    activeAutofillListElement.scrollIntoView();
+                }
+            }
+            
+        }
+    });
+    
+    inputElement.addEventListener('keyup', function (pressed) {
+        if(autofillTerms.length > 0)
+        {
+            pressed.preventDefault();
+        }
+        if (!solved && pressed.key != 'Tab' && pressed.key != 'ArrowUp' && pressed.key != 'ArrowDown' && inputElement.value.length > 1) 
+        {
+            displayAutofill();
+        }
+        if(pressed.key === 'Backspace' && inputElement.value.length < 2)
+        {
+            setAutofillActive(false);
+        }
+    });
+
+    inputElement.addEventListener('click', function()
+    {
+        if(inputElement.value.length > 1)
+        {
+            displayAutofill();
+        }
+    });
+
+    window.addEventListener('click', function(onClick)
+    {
+        if(onClick.target != autofillElement && onClick.target != inputElement)
+        {
+            setAutofillActive(false);
+        }
+    });
+}
+
+function displayAutofill()
+{
+    setAutofillActive(true);     
+    autofillElement.innerHTML = '';
+        let autofillList = '';
+        autofillTerms = matchToAutofill(inputElement.value);
+        const ul = document.createElement('ul');
+        autofillElement.appendChild(ul);
+
+        for (let i = 0; i < autofillTerms.length; i++) {
+            const li = document.createElement('li');
+            li.id = 'li' + i;
+            li.innerHTML = autofillTerms[i];
+            ul.appendChild(li);
+        }
+
+        if (autofillTerms.length > 0) {
+            setActiveAutofillElement(0);
+            for (let i = 0; i < autofillTerms.length; i++) {
+                document.getElementById('li' + i).addEventListener('mouseover', function () {
+                    setActiveAutofillElement(i);
+                });
+                document.getElementById('li' + i).addEventListener('click', function () {
+                    fillAutofillWithActiveTerm();
+
+                });
+            }
+        }
+}
+function matchToAutofill()
+{
+    if(inputElement.value.length < 2) 
+    {
+        setAutofillActive(false);
+        return [];
+    }
+
+    let reg = new RegExp(inputElement.value);
+
+    autofillElement.classList.add('active');
+
+    return autofillMapList.filter(function(term) {
+        if(term.match(reg))
+        {
+            return term;
+        }
+    });
+}
+
+function setActiveAutofillElement(id)
+{
+    if(activeAutofillListElement.classList.contains('active')) //remove current if it exists
+    {
+        activeAutofillListElement.classList.remove('active');
+    }
+    activeAutofillListID = id;
+    activeAutofillListElement = document.getElementById('li' + id);
+    activeAutofillListElement.classList.add('active'); //add new
+}
+
+function fillAutofillWithActiveTerm()
+{
+    inputElement.value = activeAutofillListElement.innerHTML;
+    setAutofillActive(false);
+    inputElement.focus();
+}
+
+function setAutofillActive(bool)
+{
+    if(bool && !autofillElement.classList.contains('active'))
+    {
+        autofillElement.classList.add('active');
+    }
+    else if(!bool && autofillElement.classList.contains('active'))
+    {
+        autofillElement.classList.remove('active');
+        if(activeAutofillListElement.classList.contains('active'))
+        {
+            activeAutofillListElement.classList.remove('active');
+            autofillTerms = [];
+            autofillElement.innerHTML = '';
+        }
+    }
+}
